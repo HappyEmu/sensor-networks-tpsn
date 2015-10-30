bdd6bce5b48f68637cbc5adde5f37cbca17860da
Urs Gerber <ug.gerber@gmail.com>
2015-10-30 13:30:06 +0100

    Remove comments, introduce header file

---
+++ tpsn.h
@@ -0,0 +1,15 @@
+#ifndef TPSN_H_
+#define TPSN_H_
+
+rimeaddr_t getDestAddr(unsigned short);
+void send_discovery_message();
+void initiate_discovery();
+
+
+typedef struct {
+	uint8_t level;
+	uint16_t broadcast_id;
+	uint8_t sender_id;
+} DiscoveryMessage;
+
+#endif
